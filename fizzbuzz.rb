abdcc2c1ad8f84b251effa1bb5815f04c5990743
HeebaShaikh <heeba126@gmail.com>
2018-07-25 15:20:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,12 @@
 #% is called a module
 
 def fizzbuzz(number)
-if number % 3 == 0 
-  puts "Fizz"
-end
+  if number % 3 == 0 
+    puts "Fizz"
+  elsif number % 5 == 0 
+  end
 end
 
 fizzbuzz(3)
+
+
